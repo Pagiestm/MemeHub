926287df70bd54ddadcd0ac972cd129aa4496405
Thibaut009 <gabet.thibaut@gmail.com>
2023-09-19 13:48:59 +0200

    update

---
+++ app/components/popup/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+export default function Popup() {
+    return (
+      <main>
+        <h1 className={styles.title}>Popup</h1>
+      </main>
+    )
+  }
+  
