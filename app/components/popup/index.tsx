c9db8f254774866919b5210d25b85cf73054b505
Thibaut009 <gabet.thibaut@gmail.com>
2023-09-19 16:47:18 +0200

    custom popup

--- app/components/popup/index.tsx
+++ app/components/popup/index.tsx
@@ -2,11 +2,12 @@
 import { useEffect, useState } from "react";
 import Image from 'next/image'
 import styles from './styles.module.css'
-import memeUp from '../../assets/img/logo.png'
-
+import logo from '../../assets/img/logo.png'
+import harold from '../../assets/img/harold.png'
 
 export default function Popup() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showHarold, setShowHarold] = useState(false);
 
   const openPopup = () => {
     setIsOpen(true);
@@ -14,6 +15,10 @@ export default function Popup() {
 
   const closePopup = () => {
     setIsOpen(false);
+  };
+
+  const handleEntrerClick = () => {
+    setShowHarold(true);
   };
 
   useEffect(() => {
@@ -25,7 +30,7 @@ export default function Popup() {
       {isOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
           <div className={styles.popup_container}>
-            <Image className={styles.logo} src={memeUp} alt="Picture"/>
+            <Image className={styles.logo} src={logo} alt="Picture"/>
             <h2 className={styles.popup_title}>Avez-vous de l’humour ?</h2>
             <div className={styles.popup_content}>
                 <label>MêmeHub est une communauté qui offre du contenu réservé aux étudiants.</label>
@@ -33,11 +38,20 @@ export default function Popup() {
             </div>
             <button
               className={styles.popup_close_button}
-              onClick={closePopup}
+              onClick={handleEntrerClick} // Utilisez le gestionnaire d'événements pour afficher Harold
             >
               J’ai de l’humour ou plus - Entrer
             </button>
+
           </div>
+          {showHarold && (
+                <Image
+                    onClick={closePopup}
+                    className={`${styles.logo} ${styles.appear_from_bottom}`} // Ajoutez la classe d'animation
+                    src={harold}
+                    alt="Picture"
+                />
+            )}
         </div>
       )}
     </div>
