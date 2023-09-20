daa9f7a1a91eda1f78ed8b528d8e100a4fa3ea0c
itsmelouis <contact@kozou.dev>
2023-09-20 12:09:16 +0200

    correction affichage

--- app/components/memes/index.jsx
+++ app/components/memes/index.jsx
@@ -41,7 +41,7 @@ function Memes() {
   return (
     <div className="container mx-auto p-4">
       <h1 className={styles.popup_title}>Liste des Memes</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {getCurrentPageItems().map((meme) => (
           <div key={meme.id} className="rounded shadow p-4">
             <Image
