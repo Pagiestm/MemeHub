36ded8037c1667a91de0e21d010758cd932943f9
pagiestm <pagiestm@gmail.com>
2023-09-20 15:27:08 +0200

    feat(Draw): add new page for draw

---
+++ app/Draw/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Header from "../components/header";
+//import Draws from "../components/Draw";
+
+export default function Draw() {
+  return (
+    <main>
+      <Header></Header>
+    </main>
+  );
+}
--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -19,7 +19,7 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 justify-center">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-[#fe9102]"
                   aria-current="page"
                 >
@@ -28,7 +28,7 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/memes"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-[#fe9102] dark:hover:bg-gray-700 dark:hover:text-[#fe9102] lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Memes
@@ -36,10 +36,10 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/Draw"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-[#fe9102] dark:hover:bg-gray-700 dark:hover:text-[#fe9102] lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Je sais pas
+                  Crée ton Dessin !
                 </a>
               </li>
               <li>
