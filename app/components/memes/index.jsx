68cfe16a5e0619febc7a85f783727a1c63c537e8
pagiestm <pagiestm@gmail.com>
2023-09-20 16:31:43 +0200

    feat(memes/index.jsx): add more page and row for pagination

--- app/components/memes/index.jsx
+++ app/components/memes/index.jsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 function Memes() {
   const [memes, setMemes] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
+  const itemsPerPage = 8;
   const [audioSrc, setAudioSrc] = useState(null);
+  const maxPaginationButtons = 10;
 
   useEffect(() => {
     // Fonction pour effectuer l'appel à l'API
@@ -49,8 +50,73 @@ function Memes() {
 
   const totalPages = Math.ceil(memes.length / itemsPerPage);
 
+  // Fonction pour gérer le changement de page
   const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
+    if (newPage < 1) {
+      setCurrentPage(1);
+    } else if (newPage > totalPages) {
+      setCurrentPage(totalPages);
+    } else {
+      setCurrentPage(newPage);
+    }
+  };
+
+  // Fonction pour afficher la pagination
+  const renderPagination = () => {
+    const pageButtons = [];
+
+    // Calcul de la première page à afficher
+    let startPage = currentPage - Math.floor(maxPaginationButtons / 2);
+    if (startPage < 1) {
+      startPage = 1;
+    }
+    let endPage = startPage + maxPaginationButtons - 1;
+    if (endPage > totalPages) {
+      endPage = totalPages;
+      startPage = endPage - maxPaginationButtons + 1;
+    }
+
+    if (startPage > 1) {
+      // Ajout d'une flèche pour revenir en arrière si nécessaire
+      pageButtons.push(
+        <button
+          key="previous"
+          className="mx-2 px-2 bg-slate-900 text-lg hover:bg-slate-700 text-white"
+          onClick={() => handlePageChange(startPage - 1)}
+        >
+          &laquo;
+        </button>
+      );
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageButtons.push(
+        <button
+          key={i}
+          className={`mx-2 px-2 bg-slate-900 text-lg hover:bg-slate-700 text-white ${
+            currentPage === i ? " border border-[#fe9102] text-[#fe9102]" : ""
+          }`}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    if (endPage < totalPages) {
+      // Ajout d'une flèche pour avancer vers la suite des pages si nécessaire
+      pageButtons.push(
+        <button
+          key="next"
+          className="mx-2 px-2 bg-slate-900 text-lg hover:bg-slate-700 text-white"
+          onClick={() => handlePageChange(endPage + 1)}
+        >
+          &raquo;
+        </button>
+      );
+    }
+
+    return pageButtons;
   };
 
   return (
@@ -62,8 +128,8 @@ function Memes() {
             <Image
               src={meme.url}
               alt={meme.name}
-              width={250} // Largeur maximale pour chaque image
-              height={250} // Hauteur maximale pour chaque image
+              width={250}
+              height={250}
               className="max-w-full h-auto mb-2"
             />
             <p className="text-center text-white">{meme.name}</p>
@@ -71,19 +137,7 @@ function Memes() {
         ))}
       </div>
       <div className="mt-4 flex justify-center">
-        {Array.from({ length: totalPages }, (_, i) => (
-          <button
-            key={i + 1}
-            className={`mx-2 px-2 bg-slate-900 text-lg hover:bg-slate-700 text-white ${
-              currentPage === i + 1
-                ? " border border-[#fe9102] text-[#fe9102]"
-                : ""
-            }`}
-            onClick={() => handlePageChange(i + 1)}
-          >
-            {i + 1}
-          </button>
-        ))}
+        {renderPagination()}
       </div>
     </div>
   );
