da4a8ee51910b82c372e5fbb1ed70ae4473e611e
pagiestm <pagiestm@gmail.com>
2023-09-20 15:37:11 +0200

    fix(header/index.tsx): modif route and class css

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -20,17 +20,17 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 justify-center">
               <li>
                 <a
-                  href="/"
-                  className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-[#fe9102]"
+                  href="/home"
+                  className={styles.link}
                   aria-current="page"
                 >
-                  Accueil
+                  Notre selection
                 </a>
               </li>
               <li>
                 <a
                   href="/memes"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-[#fe9102] dark:hover:bg-gray-700 dark:hover:text-[#fe9102] lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={styles.link}
                 >
                   Memes
                 </a>
@@ -38,7 +38,7 @@ export default function Header() {
               <li>
                 <a
                   href="/Draw"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-[#fe9102] dark:hover:bg-gray-700 dark:hover:text-[#fe9102] lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className={styles.link}
                 >
                   Crée ton Dessin !
                 </a>
@@ -48,7 +48,7 @@ export default function Header() {
                   href="#"
                   className={styles.link}
                 >
-                  Contact peut être ?
+                  a
                 </a>
               </li>
             </ul>
