bcce3844ff60b6c08b47b36a755c4c3815ebecd6
itsmelouis <contact@kozou.dev>
2023-09-21 14:54:50 +0200

    ajout de l'animation harold

--- app/components/home/index.jsx
+++ app/components/home/index.jsx
@@ -12,23 +12,41 @@ import coffinImage from "app/assets/img/coffin.gif";
 import zemmourImage from "app/assets/img/zemmour.jpeg";
 import catImage from "app/assets/img/cat.gif";
 import thunesImage from "app/assets/img/thunes.jpeg";
+import harold2Image from "app/assets/img/harold2.jpeg";
 
 export default function Home() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isHaroldScreamer, setIsHaroldScreamer] = useState(false);
+
+  const haroldScreamer = () => {
+    setIsHaroldScreamer(true);
+    setTimeout(() => setIsHaroldScreamer(false), 2000);
+  };
+
   const images = [
-    { alt: "AH", src: ahImage, className: styles.imageHarold },
-    { alt: "Hide the Pain Harold", src: haroldImage, className: styles.imageHarold },
+    {
+      alt: "AH",
+      src: ahImage,
+      className: styles.imageHarold,
+    },
+    {
+      alt: "Hide the Pain Harold",
+      src: haroldImage,
+      className: styles.imageHarold,
+
+      onClick: haroldScreamer,
+      srcSecond: harold2Image,
+    },
     { alt: "Nice", src: niceImage, className: styles.imageNice },
     { alt: "Wii Sport", src: wiiImage, className: styles.imageWii },
     { alt: "Among Us", src: amongusImage, className: styles.imageAmongUs },
     { alt: "J'suis bieng", src: jsuisBiengImage, className: styles.imageBieng },
     { alt: "Coffin Dance", src: coffinImage, className: styles.imageCoffin },
     { alt: "Zemmour", src: zemmourImage, className: styles.imageZemmour },
-    { alt: "Nyan Cat", src: catImage , className: styles.imageCat},
-    { alt: "Thunes", src: thunesImage, className: styles.imageThunes},
+    { alt: "Nyan Cat", src: catImage, className: styles.imageCat },
+    { alt: "Thunes", src: thunesImage, className: styles.imageThunes },
   ];
-
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(false);
 
   const handlePrevClick = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -46,6 +64,17 @@ export default function Home() {
 
   return (
     <div className={`container ${styles.carouselContainer}`}>
+      {isHaroldScreamer && (
+        <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center z-9999">
+          <Image
+            className=""
+            src={images[currentImageIndex].srcSecond}
+            width={1000}
+            height={1000}
+            alt="Scremaer harold"
+          />
+        </div>
+      )}
       <h1 className={styles.popup_title}>Liste des Memes</h1>
       <div className="flex justify-center items-center flex-col">
         <Image
@@ -53,16 +82,17 @@ export default function Home() {
           alt={images[currentImageIndex].alt}
           width={500}
           height={500}
-          className={`max-w-full h-auto mb-2 relative ${
+          className={`${
             isAnimating ? images[currentImageIndex].className : ""
           }`}
+          onClick={images[currentImageIndex].onClick}
         />
       </div>
       <p className={`text-center ${styles.imageText}`}>
         {images[currentImageIndex].alt}
       </p>
       <div className={`mt-4 flex justify-center ${styles.buttonsContainer}`}>
-      <button onClick={handlePrevClick} className={styles.carouselButton}>
+        <button onClick={handlePrevClick} className={styles.carouselButton}>
           Précédent
         </button>
         <button onClick={handleNextClick} className={styles.carouselButton}>
