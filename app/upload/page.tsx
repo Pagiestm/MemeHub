c3d26de94fb37e962f853e6d72f0e0eee372bca7
itsmelouis <contact@kozou.dev>
2023-09-21 09:43:36 +0200

    réagencement de la page upload

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import Header from "../components/header";
 import UploadMeme from "../components/upload";
+import Link from "next/link";
 
 export default function Meme() {
   return (
     <main>
       <Header />
-      <form action="">
-        <div className="flex flex-col gap-8 items-center">
-          <UploadMeme />
+      <form action="" className="mt-10">
+        <div className="flex flex-col gap-3 items-center">
           <div>
             <label
               htmlFor="first_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-center"
             >
               Votre nom de meme
             </label>
@@ -24,6 +24,22 @@ export default function Meme() {
               required
             />
           </div>
+          <UploadMeme />
+          <p className="text-white">ou</p>
+          <a
+            href="https://imgflip.com/memegenerator"
+            target="_blank"
+            className="text-blue-500 hover:underline font-medium text-sm mr-2 mb-2"
+          >
+            Crée ton meme ici
+          </a>
+
+          <button
+            type="submit"
+            className="text-white bg-primary hover:bg-secondary focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+          >
+            Upload ton meme !
+          </button>
         </div>
       </form>
     </main>
